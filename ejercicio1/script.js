10dbfcf4e628c771df5f9bc4f2251ff4a97b3923
Javier Frías <javifs94@gmail.com>
2019-10-14 09:16:16 +0200

    Añadidos comentarios. Cambio de nombre a la función

--- ejercicio1/script.js
+++ ejercicio1/script.js
@@ -1,6 +1,9 @@
-mostrarMensaje(); // Funcion para mostrar el mensaje del ejercicio
+mostrar(); // Funcion para mostrar el mensaje del ejercicio
 
-function mostrarMensaje() {
+/**
+ * Muestra los mensajes por consola
+ */
+function mostrar() {
     console.log("Soy un mensaje de alerta"); // Al cargar la página muestra el mensaje
     console.log("Soy el primer script"); // Al cargar la página muestra el mensaje, despues del anterior
 }
