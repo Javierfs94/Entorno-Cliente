6a9cd114e1128279ca859a7ed7b7ae144f091fe5
Javier Frías Serrano <javifs94@gmail.com>
2019-10-17 17:43:00 +0200

    Añadir ejercicios 14, 15 y 16

---
+++ ejercicio14/script.js
@@ -0,0 +1,12 @@
+function muestraOculta() {
+    let elemento = document.getElementById('contenidos_' + id);
+    let enlace = document.getElementById('enlace_' + id);
+
+    if (elemento.style.display == "" || elemento.style.display == "block") {
+        elemento.style.display = "none";
+        enlace.innerHTML = 'Mostrar contenidos';
+    } else {
+        elemento.style.display = "block";
+        enlace.innerHTML = 'Ocultar contenidos';
+    }
+}
