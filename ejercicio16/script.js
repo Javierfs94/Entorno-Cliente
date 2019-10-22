2be2219c9a618a10f44958d0d705cd2e08457b0f
Javier Frías Serrano <javifs94@gmail.com>
2019-10-22 20:05:10 +0200

    Comprobaciones eliminadas ya que son innecesarias close #3

--- ejercicio16/script.js
+++ ejercicio16/script.js
@@ -14,17 +14,6 @@ function muestraInformacion(mensaje) {
 function tamanoVentanaNavegador() {
     let dimensiones = [];
 
-    if (typeof(window.innerWidth) == 'number') {
-        // No es IE
-        dimensiones = [window.innerWidth, window.innerHeight];
-    } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
-        //IE 6 en modo estandar (no quirks)
-        dimensiones = [document.documentElement.clientWidth, document.documentElement.clientHeight];
-    } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
-        //IE en modo quirks
-        dimensiones = [document.body.clientWidth, document.body.clientHeight];
-    }
-
     return dimensiones;
 }
 
