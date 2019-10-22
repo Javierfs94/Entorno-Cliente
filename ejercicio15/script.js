c5d6ca4e965b4ad504f2c4414662171bafbb2235
Javier Frías Serrano <javifs94@gmail.com>
2019-10-22 19:58:01 +0200

    Var cambiado por let close #4

--- ejercicio15/script.js
+++ ejercicio15/script.js
@@ -1,6 +1,6 @@
 function muestraInformacion(mensaje) {
     document.getElementById("info").innerHTML = '<h1>' + mensaje[0] + '</h1>';
-    for (var i = 1; i < mensaje.length; i++) {
+    for (let i = 1; i < mensaje.length; i++) {
         document.getElementById("info").innerHTML += '<p>' + mensaje[i] + '</p>';
     }
 }
